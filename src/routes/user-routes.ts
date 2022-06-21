623e537dfa21ba4ec02aab70314e0c25df092399
Liandro Silva <liandro.silva1012@gmail.com>
2022-06-20 21:56:22 -0300

    feat: added middlewares to routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,12 +1,21 @@
-import { validateBodyParams } from './../middlewares/user-create-middleware';
+import { userSigninValidateRequestMiddleware } from './../middlewares/user-signin-middleware';
+import { userSignupValidateRequestMiddleware } from './../middlewares/user-create-middleware';
 import { Router } from 'express';
 import { signup, signin, reSignin } from '../controllers/user-controller';
 import AuthService from '../services/auth-service';
 
 const router = Router();
 
-router.post('/authenticate/signup', validateBodyParams, signup);
-router.post('/authenticate/signin', signin);
+router.post(
+  '/authenticate/signup',
+  userSignupValidateRequestMiddleware,
+  signup
+);
+router.post(
+  '/authenticate/signin',
+  userSigninValidateRequestMiddleware,
+  signin
+);
 router.post('/authenticate/re-signin', AuthService.authorize, reSignin);
 
 export default router;
